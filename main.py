cbb368ecb6e9fffd21cac55a5039b60404b209da
Yuki <32415464+yukikamome316@users.noreply.github.com>
2024-01-16 16:41:24 +0900

    feat: dotenvの追加

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+
+# .envファイルの読み込み
+load_dotenv()
 
 options = webdriver.ChromeOptions()
 options.add_argument('--disable-gpu');
@@ -7,7 +13,11 @@ options.add_argument('--disable-extensions');
 options.add_argument('--start-maximized');
 options.add_experimental_option("excludeSwitches", ['enable-automation'])
 
-chromedriver_path = './chromedriver.exe'
+chromedriver_path = os.getenv('CHROMEDRIVER_PATH')
+obicnet_url = os.getenv('OBICNET_URL')
+
 service = Service(executable_path=chromedriver_path)
 driver = webdriver.Chrome(service=service, options=options)
-print(type(driver))
+driver.get(obicnet_url)
+
+
