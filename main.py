2117158a0385a105512a5241a1916c9564a5dccb
Yuki <32415464+yukikamome316@users.noreply.github.com>
2024-01-17 02:38:35 +0900

    feat: 予約可能日時一覧まで表示

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ e_login_page_button.click()
 
 wait = WebDriverWait(driver, 10)
 
+# Ajaxでiframeが生成されるので待機
 e_iframe = wait.until(EC.presence_of_element_located((By.ID, 'frameMenu')))
 driver.switch_to.frame(e_iframe)
 
@@ -44,5 +45,33 @@ e_password = driver.find_element(By.ID, 'txtPassword')
 e_kyosyusei_id.send_keys(student_id)
 e_password.send_keys(password)
 
+# ログインボタンをクリック
 e_login_button = driver.find_element(By.ID, 'btnAuthentication')
 e_login_button.click()
+
+# 教習予約(指導員)
+e_reserve_nominated = wait.until(EC.presence_of_element_located((By.ID, 'btnMenu_KyoushuuYoyaku_Sidouin')))
+e_reserve_nominated.click()
+
+# TODO: ここのハードコーディングは絶対に直すこと
+teachers = ["指導員A", "指導員B", "指導員C"]
+
+assert len(teachers) <= 3
+
+for i in range(len(teachers)):
+  e_search_select_button = wait.until(EC.presence_of_element_located((By.ID, 'btnSearch' + str(i + 1))))
+  e_search_select_button.click()
+
+  e_search_text_input = wait.until(EC.presence_of_element_located((By.ID, 'txtSearchText')))
+  e_search_text_input.send_keys(teachers[i])
+  e_search_button = driver.find_element(By.ID, 'btnSearch')
+  e_search_button.click()
+
+  # print(driver.page_source)
+  e_teacher_select_input = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='page'][@index='0']")))
+  e_teacher_select_input.click()
+
+e_button_commit = wait.until(EC.presence_of_element_located((By.ID, 'btnCommit')))
+e_button_commit.click()
+
+# driver.save_screenshot('screenshot.png')
